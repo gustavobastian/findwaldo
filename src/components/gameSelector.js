6bd9aed8f176690dee9cc8bff2cf1cb800c0233a
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-05-19 12:35:02 -0300

    adding selector of game

--- src/components/gameSelector.js
+++ src/components/gameSelector.js
@@ -1,10 +1,25 @@
 import React from "react";
+import '../styles/selector.css'
+import { useState } from "react";
 
 const Selector = (props)=>{
 
+    const [state,setState]=useState(2)
+
+
+    const updateState= (value)=>{
+        console.log(value);
+    };
+
+
     return(
         <div>
-        Selector
+            <label for="select">Game:</label>            
+            <select className="gameSelector" name="select">
+                <option value="value1" selected>Game 1</option>
+                <option value="value2">Game 2</option>
+                <option value="value3">Game 3</option>
+            </select>
         </div>
     )
 
