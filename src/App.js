137c3a6677336d44306bc4e125fe8594796727b6
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-05-19 13:34:37 -0300

    select between different images

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './pages/main'
 import About from './pages/about'
 import Error from './pages/errorPage'
+import { useState,useReducer } from 'react';
 import './App.css';
 
 
 function App() {
+  const [currentGame,setCurrentGame]=useState(1);
+  const [ignored,forceupdate]=useReducer(x=>x+1,0);
+
+  const setCurrentGameLocal =(value)=>{
+        setCurrentGame(value);
+        forceupdate();        
+  }
   
   return (
     <BrowserRouter>      
     <Routes>       
-    <Route path="/" element={<Main />} />
+    <Route path="/" element={<Main valueCurrent={currentGame} setCurrentGame={(e)=>setCurrentGameLocal(e)}/>} />
     <Route path="/about" element={<About />} />
     <Route path="/*" element={<Error />} />
     </Routes>  
