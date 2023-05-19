cb3ad7cb8df500349b10652647a4b97f9b1986ae
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-05-19 17:55:32 -0300

    working in click check

--- src/components/gameBody.js
+++ src/components/gameBody.js
@@ -1,25 +1,88 @@
 import React  from 'react';
 import '../styles/gameClass.css'
+import { useState,useReducer } from 'react';
 
-const gameFun = (props)=>{
-    
+const GameFun = (props)=>{
+
+    const [state,setState]= useState(false,false,false);
+    const [state2,setState2]= useState([
+        1,
+        1,
+        1,           
+    ]);
+    const [ignored,forceupdate]=useReducer(x=>x+1,0);
+    /**only for dev, image 1 */
+    const positions=[
+        {"x":189 ,"y":185},        
+        {"x":302 ,"y":298},
+        {"x":120 ,"y":62},
+    ]
+
+    const getPoint= (e)=>{    
+        
+        if(!e){return;}
+        
+        let currentPosition=[e.clientX,e.clientY]
+        console.log("state:"+JSON.stringify(state2))
+        console.log("state:"+JSON.stringify(state2[0]))
+        
+        if((Math.abs(currentPosition[0]-positions[0].x)<10)&&(Math.abs(currentPosition[1]-positions[0].y)<10)){
+            console.log("1 ok")
+            setState2([
+                0.5,
+                state2[1],
+                state2[2],                         
+            ])        
+            forceupdate(); 
+            
+        }
+        
+        if((Math.abs(currentPosition[0]-positions[1].x)<10)&&(Math.abs(currentPosition[1]-positions[1].y)<10)){
+            console.log(" 2 ok")
+            ///setState(state[0],true,state[2])
+            setState2([
+                state2[0],
+                0.5,
+                state2[2],                         
+            ])        
+        
+            
+        
+        }
+        
+        if((Math.abs(currentPosition[0]-positions[2].x)<10)&&(Math.abs(currentPosition[1]-positions[2].y)<10)){
+            console.log(" 3 ok")
+            //setState(state[0],state[1],true)
+            setState2([
+                state2[0],
+                state2[1],
+                0.5,                         
+            ])        
+            forceupdate();     
+            
+        }
+    }
+    /*style={{opacity: state2[1]}} */
     return(
         <div className="gameClass">
-          <img className="imageBody" src={require("../images/img"+(parseInt(props.value))+".webp")} alt="not found">           
+          <img className="imageBody" src={require("../images/img"+(parseInt(props.value))+".webp")} alt="not found" onClick={(e)=>{getPoint(e);}}>           
            </img>
            <div className='objetives'>
-            <div className="objetive">
-                <img className="objetiveImage"  src={require("../images/objetive"+(parseInt(props.value))+"/icon1.png")} alt="not found"></img>
-            </div>
-            <div className="objetive">
-                <img className="objetiveImage"  src={require("../images/objetive"+(parseInt(props.value))+"/icon2.png")} alt="not found"></img>
-            </div>
-            <div className="objetive">
-                <img className="objetiveImage" src={require("../images/objetive"+(parseInt(props.value))+"/icon3.png")} alt="not found"></img>
-            </div>
+                <div className="targetName">Objetive 1</div>
+                <div className="objetive"  style={{opacity: state2[0]}}>                
+                    <img className="objetiveImage" src={require("../images/objetive"+(parseInt(props.value))+"/icon1.png")} alt="not found"></img>
+                </div>
+                <div className="targetName">Objetive 2</div>
+                <div className="objetive" style={{opacity: state2[1]}} >            
+                    <img   className="objetiveImage"  src={require("../images/objetive"+(parseInt(props.value))+"/icon2.png")} alt="not found"></img>
+                </div>
+                <div className="targetName">Objetive 3</div>
+                <div className="objetive" style={{opacity: state2[2]}}>                
+                    <img  className="objetiveImage" src={require("../images/objetive"+(parseInt(props.value))+"/icon3.png")} alt="not found"></img>
+                </div>
            </div>
         </div>
     );
 };
 
-export default gameFun;
+export default GameFun;
